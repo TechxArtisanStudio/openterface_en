eb10d0c1ee8459759f964f6ddaa30e3b83a6785f
Billy Wang <billy@techxartisan.com>
2026-08-19 22:31:44 +0800

    Replace Astro redirect with branded redirect page for /day_glow_launch/

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,6 @@ export default defineConfig({
     '/community/': 'https://forum.openterface.com/',
     '/appointment/': 'https://calendar.app.google/9nyjjpA9m3TUpzsaA',
     '/support/appointment/': 'https://calendar.app.google/9nyjjpA9m3TUpzsaA',
-    '/day_glow_launch/': 'https://www.youtube.com/playlist?list=PLoHh3GdjF7AGJ-dg4lmwdSNmVCPBr7aOn',
   },
   // site-refactor-redirects:end
   integrations: [
